9e18616ce3486ad0848eb691d7e785e586ef3614
rihanazar1 <rihanazhar5@gmail.com>
2025-05-12 09:46:53 +0530

    practice continue

--- questions/JSimpConcepts.js
+++ questions/JSimpConcepts.js
@@ -114,5 +114,43 @@
   
   
 
+// __________________________________________________
+
+// console.log("start of script3")
+
+// setTimeout(()=>{
+//     console.log("This is from the Task Queue (setTimeout)")
+// },0)
+
+// Promise.resolve().then(() =>{
+//     console.log("This is from the Microtask Queue (Promises)")
+// })
+
+// console.log("End of Script");
+
+
+// function Person(fname, lname, pnumber){
+//     this.fname = fname;
+//     this.lname = lname;
+//     this.pnumber = pnumber;
+
+//     this.getName = function (){
+//         console.log(this.fname, this.lname)
+//     }
+// }
+
+// let person1 = new Person("Rihan", "Azhar", "9999999")
+// person1.getName() 
+
+
+// function add(a, b, cb){
+//     let result = a + b;
+//     cb(result);
+// }
+
+
+// add(2, 4, function(val){
+//     console.log(val)
+// })
 
 
--- questions/tempCodeRunnerFile.js
+++ questions/tempCodeRunnerFile.js
@@ -1,38 +1,165 @@
-// console.log("start of script3")
+// let a = 5 
+// let str = ""
 
-// setTimeout(()=>{
-//     console.log("This is from the Task Queue (setTimeout)")
-// },0)
+// for(let i=1; i<=a; i++){
+//     str = str + i + " "
+// }
+// console.log(str)
 
-// Promise.resolve().then(() =>{
-//     console.log("This is from the Microtask Queue (Promises)")
-// })
+// let num = 5
+// let sum = 0
 
-// console.log("End of Script");
+// for(let i=1; i<=num; i++){
+//     sum = sum + i
+// }
+// console.log(sum)
 
 
-// function Person(fname, lname, pnumber){
-//     this.fname = fname;
-//     this.lname = lname;
-//     this.pnumber = pnumber;
+// let num = 5;
+// let fac = 1
 
-//     this.getName = function (){
-//         console.log(this.fname, this.lname)
+// for(let i=1; i<=num; i++){
+//     fac = fac * i
+// }
+
+// console.log(fac)
+
+// let num = 7
+
+// let count = 0
+
+// for(let i=1; i<=num; i++){
+//     if(num % i == 0){
+//         count++
 //     }
 // }
 
-// let person1 = new Person("Rihan", "Azhar", "9999999")
-// person1.getName() 
+// console.log(count === 2 ? `${num} is a Prime Number` : "Not Prime"  )
 
 
-// function add(a, b, cb){
-//     let result = a + b;
-//     cb(result);
+// let num = 1234
+// let sum = 0
+
+// while(num > 0){
+//     let mod = num % 10
+//     sum = sum + mod 
+//     num = Math.floor(num / 10)
 // }
 
+// console.log(sum)
 
-// add(2, 4, function(val){
-//     console.log(val)
-// })
+
+// let num = 36798
+// let rev = 0
+
+// while(num > 0){ 
+//     let mod = num % 10
+//     rev = rev * 10 + mod
+//     num = Math.floor(num / 10) 
+// }
+// console.log(rev) 
+
+
+// let num = 12321
+// let copy = num
+// let rev = 0
+
+// while(num > 0){
+//     let mod = num % 10
+//     rev = rev * 10 + mod 
+//     num = Math.floor(num / 10)
+// }
+
+// console.log(rev == copy ? "Plindrome" : "Not Palindrome")
+
+
+// let num = 24
+// let copy = num
+// let ans = 0
+
+// while(num > 0){
+//     let mod = num % 10
+//     let fac = 1
+//     for(let i =1; i<=mod; i++){
+//         fac = fac * i
+//     }
+//     ans = ans + fac 
+//     num = Math.floor(num / 10)
+// }
+// console.log(copy === ans ? "Strong Number" : "Not Strong")
+
+
+// let num = 145 
+// let copy = num
+// let sum = 0
+
+// while(num > 0){
+//     let mod  = num % 10 
+//     let fac = 1
+//     for(let i=1; i<=mod; i++){
+//         fac = fac * i 
+//     }
+//     sum = sum + fac 
+//     num = Math.floor(num / 10)
+// }
+
+// console.log(copy == sum ? "Strong Number" : "Not Strong")
+
+
+// let num = 25 
+// let copy = num
+// let sq = num * num 
+// let count = 0
+
+// while(num > 0){
+//     count++
+//     num = Math.floor(num / 10)
+// }
+
+// console.log(copy === sq % Math.pow(10, count) ? "Automorphic" : "Not Automorphic")
+
+
+
+// let num = 6758 
+// let sum = 0 
+
+// while(num > 0 || sum > 9){
+//     if(num == 0){
+//         num = sum 
+//         sum = 0
+//     }
+//     sum = sum + num%10
+//     num = Math.floor(num / 10)
+// }
+// console.log(sum)
+
+// let num = 11
+// let sum = 0 
+
+// while(num > 0 || sum > 9){
+//     if(num == 0){
+//         num = sum 
+//         sum = 0
+//     }
+//     sum = sum + num%10
+//     num = Math.floor(num / 10)
+// }
+// console.log(sum)
+
+
+// let num = 25 
+// let copy = num
+// let count = 0
+// let sq = num * num 
+
+// while(num > 0){
+//     count++
+//     num = Math.floor(num / 10)
+// }
+
+// console.log(copy == sq % Math.pow(10, count) ? "Automorphic" : "Not Automorphic")
+
+
+
 
 
